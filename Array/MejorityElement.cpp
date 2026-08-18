c3bada25023722586a4aed88ab0f789d03ba408f
rupesh <londherupesh72@gmail.com>
2026-08-18 18:32:38 +0530

    mejority element

--- Array/MejorityElement.cpp
+++ Array/MejorityElement.cpp
@@ -1,32 +1,62 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
-vector<int> pairSum(vector<int>& arr, int target) {
-    vector<int> result;
-    int n = arr.size();
-    int i = 0, j = n - 1;
-    int ps;
+
+// vector<int>pairSum(vector<int> nums,int target){
+//     vector<int> ans;
+//     int n = nums.size();
+
+//     for(int i=0;i<n;i++){
+//         for(int j=n-1;j>i;j--){
+//             if(nums[i]+nums[j]==target){
+//                 ans.push_back(i);
+//                 ans.push_back(j);
+//             }
+//         }
+
+//     }
+//     return ans;
+
+// }
+
+vector<int>pairSum(vector<int> nums,int target){
+    vector<int> ans;
+    int n = nums.size();
+    int i=0;
+    int j=n-1;
+
     while(i<j){
-        ps = arr[i] + arr[j];
-    }if(ps > target){
-        j--;
-    }else if (ps < target)
-    {
-        i++;
-    }else{
-        result.push_back(i);
-        result.push_back(j);
-        return result;
-    }
-    
+            if(nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+
+            }
+            else if(nums[i]+nums[j]<target){
+                i++;
+
+            }
+            else{
+                j--;
+            }
+        }
+        
+    return ans;
+
 }
+
 int main(){
-    vector<int> nums = {2, 7, 11, 15};
+    vector<int> nums = {2,7,21,13};
     int target = 9;
-
-    vector<int> ans = pairSum(nums, target);
-    cout << ans[0] << " " << ans[1] << endl;
-
+    vector<int> ans =pairSum(nums,target);
+     if (ans.size() == 2)
+    {
+        cout << ans[0] << " " << ans[1] << endl;
+    }
+    else
+    {
+        cout << "Pair not found" << endl;
+    }
     return 0;
-    
+
 }
