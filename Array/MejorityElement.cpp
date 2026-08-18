b86f01ba8f29753bb38d91441c858136a60d6138
rupesh <londherupesh72@gmail.com>
2026-08-18 18:40:23 +0530

    solution class is inserted in Mejority Element

--- Array/MejorityElement.cpp
+++ Array/MejorityElement.cpp
@@ -2,22 +2,22 @@
 #include<vector>
 using namespace std;
 
-// vector<int>pairSum(vector<int> nums,int target){
-//     vector<int> ans;
-//     int n = nums.size();
+vector<int>pairSum(vector<int> nums,int target){
+    vector<int> ans;
+    int n = nums.size();
 
-//     for(int i=0;i<n;i++){
-//         for(int j=n-1;j>i;j--){
-//             if(nums[i]+nums[j]==target){
-//                 ans.push_back(i);
-//                 ans.push_back(j);
-//             }
-//         }
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>i;j--){
+            if(nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+            }
+        }
 
-//     }
-//     return ans;
+    }
+    return ans;
 
-// }
+}
 
 vector<int>pairSum(vector<int> nums,int target){
     vector<int> ans;
@@ -44,9 +44,29 @@ vector<int>pairSum(vector<int> nums,int target){
     return ans;
 
 }
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0, ans = 0;
+        for(int i=0;i<nums.size();i++){
+            if(freq == 0){
+                ans =nums[i];
+
+            }if(ans == nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
 
 int main(){
+    Solution s;
     vector<int> nums = {2,7,21,13};
+    int majority = s.majorityElement(nums);
+    cout << "Majority Element: " << majority << endl;
     int target = 9;
     vector<int> ans =pairSum(nums,target);
      if (ans.size() == 2)
